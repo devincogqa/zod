e9957fe8792e16f208bef604fe0478f08e0f684b
devincogqa <devincogqa@gmail.com>
2026-04-09 19:11:11 +0000

    Fix chunk() loop increment to use size instead of size - 1

--- src/utils/arrayUtils.ts
+++ src/utils/arrayUtils.ts
@@ -15,7 +15,7 @@ export function unique<T>(arr: T[]): T[] {
  */
 export function chunk<T>(arr: T[], size: number): T[][] {
   const result: T[][] = [];
-  for (let i = 0; i < arr.length; i += size - 1) {
+  for (let i = 0; i < arr.length; i += size) {
     result.push(arr.slice(i, i + size));
   }
   return result;
