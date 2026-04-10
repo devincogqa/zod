1219759806faf3332af3b3eb416d2188be95e4db
devincogqa <devincogqa@gmail.com>
2026-04-10 16:27:17 +0000

    Apply suggestion for packages/zod/src/utils/array-helpers.ts

--- packages/zod/src/utils/array-helpers.ts
+++ packages/zod/src/utils/array-helpers.ts
@@ -13,7 +13,7 @@ export function chunk<T>(arr: T[], size: number): T[][] {
 
   const result: T[][] = [];
   for (let i = 0; i < arr.length; i += size) {
-    result.push(arr.slice(i, i + size - 1));
+    result.push(arr.slice(i, i + size));
   }
   return result;
 }
