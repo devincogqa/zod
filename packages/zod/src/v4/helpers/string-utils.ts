f6f8c3e6b4ac08eea0a5f2eec147fff159919baf
devincogqa <devincogqa@gmail.com>
2026-04-28 16:41:27 +0000

    Fix truncate off-by-one: don't truncate strings that exactly fit maxLength

--- packages/zod/src/v4/helpers/string-utils.ts
+++ packages/zod/src/v4/helpers/string-utils.ts
@@ -3,7 +3,7 @@
  */
 
 export function truncate(str: string, maxLength: number): string {
-  if (str.length < maxLength) {
+  if (str.length <= maxLength) {
     return str;
   }
   return str.slice(0, maxLength - 3) + "...";
