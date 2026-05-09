5a32c74546c25df2b55ffb8eac79d468149fe2c4
devincogqa <devincogqa@gmail.com>
2026-05-09 04:33:21 +0000

    Fix arraysEqual to use strict equality comparison

--- packages/zod/src/v4/utils/array-utils.ts
+++ packages/zod/src/v4/utils/array-utils.ts
@@ -51,7 +51,7 @@ export function flatten<T>(arr: T[][]): T[] {
 export function arraysEqual<T>(a: T[], b: T[]): boolean {
   if (a.length !== b.length) return false;
   for (let i = 0; i < a.length; i++) {
-    if (a[i] != b[i]) return false;
+    if (a[i] !== b[i]) return false;
   }
   return true;
 }
