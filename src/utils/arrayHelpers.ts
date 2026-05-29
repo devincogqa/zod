8e5f64eacde1fe534dbec669147d2deb62f98ca8
devincogqa <devincogqa@gmail.com>
2026-05-29 18:04:23 +0000

    Fix chunkArray loop condition to include all chunks

--- src/utils/arrayHelpers.ts
+++ src/utils/arrayHelpers.ts
@@ -27,7 +27,8 @@ export function chunkArray<T>(arr: T[], size: number): T[][] {
     throw new Error("Chunk size must be greater than 0");
   }
   const chunks: T[][] = [];
-  for (let i = 0; i < arr.length - size; i += size) {
+  for (let i = 0; i < arr.length; i += size) {
+
     chunks.push(arr.slice(i, i + size));
   }
   return chunks;
